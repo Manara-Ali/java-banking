19c467232fb5f9784197e173b226a609757f472e
Manara Ali <manaraali64@gmail.com>
2021-10-27 16:29:22 -0400

    Add service and controller methods

--- JavaBanking/src/main/java/com/manara/ali/javabanking/controller/CustomerController.java
+++ JavaBanking/src/main/java/com/manara/ali/javabanking/controller/CustomerController.java
@@ -3,6 +3,7 @@ package com.manara.ali.javabanking.controller;
 import com.manara.ali.javabanking.model.CustomerModel;
 import com.manara.ali.javabanking.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,4 +28,21 @@ public class CustomerController {
     public List<CustomerModel> getAllCustomers() {
         return customerService.getAllCustomers();
     }
+
+    @GetMapping("/customer/{id}")
+    public CustomerModel getCustomer(@PathVariable int id) {
+        return customerService.getCustomer(id);
+    }
+
+    // Create a method to update customer
+    @PatchMapping("/update-customer/{id}")
+    public CustomerModel updateCustomer(@PathVariable int id, @RequestBody CustomerModel customerModel) {
+        return customerService.updateCustomer(customerModel, id);
+    }
+
+    @DeleteMapping("/delete-customer/{id}")
+    public HttpStatus deleteCustomer(@PathVariable int id) {
+        return customerService.deleteCustomer(id);
+    }
+
 }
--- JavaBanking/src/main/java/com/manara/ali/javabanking/service/CustomerService.java
+++ JavaBanking/src/main/java/com/manara/ali/javabanking/service/CustomerService.java
@@ -3,6 +3,7 @@ package com.manara.ali.javabanking.service;
 import com.manara.ali.javabanking.model.CustomerModel;
 import com.manara.ali.javabanking.repository.CustomerModelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,5 +23,30 @@ public class CustomerService {
     // Create a method to retrieve all customers
     public List<CustomerModel> getAllCustomers() {
         return customerModelRepository.findAll();
+    }
+
+    // Create a method to retrive a single customer
+    public CustomerModel getCustomer(int id) {
+        return customerModelRepository.findById(id).get();
+    }
+
+    // Create a method to update a customer
+    public CustomerModel updateCustomer(CustomerModel customerModel, int id) {
+        // Find the current customer
+        CustomerModel customer = getCustomer(id);
+
+        customer.setFirstname(customerModel.getFirstname());
+        customer.setLastname(customerModel.getLastname());
+        customer.setUsername(customerModel.getUsername());
+        customer.setPassword(customerModel.getPassword());
+
+        // Return the customer
+        return customerModelRepository.save(customer);
+    }
+
+    // Create a method to delete a single user
+    public HttpStatus deleteCustomer(int id) {
+        customerModelRepository.deleteById(id);
+        return HttpStatus.OK;
     }
 }
