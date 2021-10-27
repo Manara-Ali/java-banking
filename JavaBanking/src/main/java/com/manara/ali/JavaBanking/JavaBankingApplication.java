b12b2ab2b1f2d66d41e9a570e7fe1274c336d7d9
Manara Ali <manaraali64@gmail.com>
2021-10-27 08:11:24 -0400

    Add new spring boot dependencies

---
+++ JavaBanking/src/main/java/com/manara/ali/JavaBanking/JavaBankingApplication.java
@@ -0,0 +1,13 @@
+package com.manara.ali.JavaBanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBankingApplication.class, args);
+	}
+
+}
---
+++ JavaBanking/src/test/java/com/manara/ali/JavaBanking/JavaBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manara.ali.JavaBanking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
